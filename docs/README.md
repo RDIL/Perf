cc2b07ce0372814ebd73b163775d225d863d7f96
Reece Dunham <me@rdil.rocks>
2020-01-10 17:14:36 +0000

    init documentation commit

---
+++ docs/README.md
@@ -0,0 +1,17 @@
+# Docs
+
+This website is built using [Docusaurus](https://v2.docusaurus.io/), a modern static website generator.
+
+### Installation
+
+```bash
+$ yarn
+```
+
+### Local Development
+
+```bash
+$ yarn start
+```
+
+This command starts a local development server and open up a browser window. Most changes are reflected live without having to restart the server.
---
+++ docs/src/pages/index.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import classnames from 'classnames';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import styles from '../home/styles.module.css';
+
+const features = [
+  {
+    title: <>Easy to Use</>,
+    imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    description: (
+      <>
+        PyPerf is designed to be easily installed and
+        used to get your Python modules running faster.
+      </>
+    ),
+  },
+  {
+    title: <>Focus on What Matters</>,
+    imageUrl: 'img/undraw_docusaurus_tree.svg',
+    description: (
+      <>
+        Write code at your own pace, and
+        we&apos;ll make it run at a great pace.
+      </>
+    ),
+  },
+  {
+    title: <>Open Source</>,
+    imageUrl: 'img/undraw_docusaurus_react.svg',
+    description: (
+      <>
+        Extend, change, and help PyPerf by sending us a PR on GitHub! 
+      </>
+    ),
+  },
+];
+
+function Feature({imageUrl, title, description}) {
+  const imgUrl = useBaseUrl(imageUrl);
+  return (
+    <div className={classnames('col col--4', styles.feature)}>
+      {imgUrl && (
+        <div className="text--center">
+          <img className={styles.featureImage} src={imgUrl} alt={title} />
+        </div>
+      )}
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+export default () => {
+  const context = useDocusaurusContext();
+  const {siteConfig = {}} = context;
+  return (
+    <Layout
+      title={"PyPerf - The CLI that helps make your apps faster."}
+      description="A CLI that checks for potential performance issues in your Python modules.">
+      <header
+        className={classnames('hero hero--primary', styles.heroBanner)}
+      >
+        <div className="container">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className={classnames(
+                'button button--outline button--secondary button--lg',
+                styles.getStarted,
+              )}
+              to={useBaseUrl('docs/doc1')}
+            >
+              Get Started
+            </Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        {features && features.length && (
+          <section className={styles.features}>
+            <div className="container">
+              <div className="row">
+                {features.map((props, idx) => (
+                  <Feature key={idx} {...props} />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
+      </main>
+    </Layout>
+  );
+}
